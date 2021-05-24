2793f52274c337ddce31fe370397ae16e96c0ed8
pigs-can-fly <edisen.zhuang@gmail.com>
2021-05-24 23:19:49 +0800

    roll back to first I DO code base

--- jest-project/src/sum.test.js
+++ jest-project/src/sum.test.js
@@ -11,19 +11,9 @@ describe("the logic of sum()", () => {
     random.mockReturnValue(0); // mock return value
     expect(testObject.sum(1, 1)).toBe(2);
   });
-});
 
-describe("the parameters validation of sum()", () => {
-  // Data type of input may not be integer, but it should be parsed as int within.
-  test("Convert string to integer", () => {
-    random.mockReturnValue(0);
-    expect(testObject.sum("1", "1")).toBe(2);
-  });
-
-  // Data type such as "apple" cannot be parsed as int.
-  test("Non numeric input to throw an error", () => {
-    expect(() => {
-      testObject.sum("apple", "orange");
-    }).toThrow(new Error("Input must be numeric"));
+  // This test will fail because 1 + 1 = 3 is wrong.
+  test("1 + 1 = 3 to fail", () => {
+    expect(testObject.sum(1, 1)).toBe(3);
   });
 });
